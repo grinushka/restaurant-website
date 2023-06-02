951e19dfe5e3904ce44683b2ffd0d7de808c11fa
grinushka <vilu.badgley@gmail.com>
2023-06-02 19:18:11 +0700

    refactor js file, delete useless icons, edit readme

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
 <h1> Restaurant Website</h1>
 
-Simple website for the imaginary restaurant. IT was created in order to:
-- improve JS Object skills
+[Project live preview]()
+
+Simple website for the imaginary restaurant. IT was created in order to improve:
+- JS Objects skills
 - DOM manipulation
-- play around with animations
+- CSS Transitions
 
 <h2> Screenshots </h2>
 
-![App Screenshot](./website-preview-2.png)
+![App Screenshot](./website-preview.png)
 
 <h2> Technologies Used </h2>
 
 - HTML5
-  <a href="https://developer.mozilla.org/ru/docs/Web/HTML" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-plain.svg" alt="html" width="40" height="40" align="center"/> </a>
+  <a href="https://developer.mozilla.org/ru/docs/Web/HTML" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-plain.svg" alt="html" width="30" height="30" align="center"/> </a>
 
 - CSS3
-  <a href="https://developer.mozilla.org/ru/docs/Learn/Getting_started_with_the_web/CSS_basics" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain.svg" alt="css" width="40" height="40" align="center"/> </a>
+  <a href="https://developer.mozilla.org/ru/docs/Learn/Getting_started_with_the_web/CSS_basics" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain.svg" alt="css" width="30" height="30" align="center"/> </a>
 
 - Webpack
-  <a href="https://webpack.js.org/" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/webpack/webpack-plain.svg" alt="html" width="40" height="40" align="center"/> </a>
+  <a href="https://webpack.js.org/" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/webpack/webpack-plain.svg" alt="html" width="30" height="30" align="center"/> </a>
 
 - SCSS
-  <a href="https://sass-lang.com/documentation/" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sass/sass-original.svg" alt="html" width="40" height="40" align="center"/> </a>
+  <a href="https://sass-lang.com/documentation/" target="_blank" rel="noreferrer"> &nbsp; <img src="https://github.com/devicons/devicon/blob/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sass/sass-original.svg" alt="html" width="30" height="30" align="center"/> </a>
 
 
 <h2> Badges </h2>
@@ -30,8 +32,6 @@ Simple website for the imaginary restaurant. IT was created in order to:
 [![Twitter](https://img.shields.io/twitter/follow/grinushka)](https://twitter.com/grinushka)
 &nbsp;
 [![Codewars](https://img.shields.io/badge/Codewars-grinushka-red)](https://www.codewars.com/users/grinushka)
-&nbsp;
-[![Vk](https://img.shields.io/badge/Vk-follow-blue)](https://vk.com/grinushka)
 &nbsp;
 
 <h2> Acknowledgements </h2>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-"use strict";
+'use strict';
 
 // Imports
 
-import "./styles/index.scss";
-import { menu } from  "./data/data-menu.js";
-import { contactsData } from "./data/data-contacts.js";
+import './styles/index.scss';
+import { menu } from './data/data-menu.js';
 
 import pasta from './assets/pasta.jpg';
 import address from './assets/location.png';
@@ -12,9 +11,9 @@ import phone from './assets/phone.png';
 import email from './assets/email.png';
 
 // Load the home tab as a default
-window.onload = function() {
+window.onload = function () {
   displayHome();
-}
+};
 
 // Event listeners
 const navHome = document.querySelector('.nav-home');
@@ -26,7 +25,7 @@ navHome.addEventListener('click', () => {
   displayHome();
   checkActiveNav();
   navHome.classList.add('nav-selected');
-})
+});
 
 navMenu.addEventListener('click', () => {
   clearMain();
@@ -34,32 +33,31 @@ navMenu.addEventListener('click', () => {
   fillInMenu('Mains', 'Pizza', 'Salads');
   checkActiveNav();
   navMenu.classList.add('nav-selected');
-})
+});
 
 navContact.addEventListener('click', () => {
   clearMain();
   displayContacts('contact');
   checkActiveNav();
   navContact.classList.add('nav-selected');
-})
+});
 
 function checkActiveNav() {
   let nav = document.querySelectorAll('.nav-item');
-  nav.forEach( el => {
-    if(el.classList.contains('nav-selected')) {
-      el.classList.remove('nav-selected')
+  nav.forEach((el) => {
+    if (el.classList.contains('nav-selected')) {
+      el.classList.remove('nav-selected');
     }
-  })
+  });
 }
 
-// DOM 
+// DOM
 
 // Create container
 const main = document.querySelector('.main');
 const container = document.createElement('div');
 container.classList.add('container', 'container-main');
 main.append(container);
-
 
 // Functions to display different tabs
 
@@ -84,24 +82,15 @@ function displayHome() {
   btnHome.classList.add('btn', 'btn-orange');
   btnHome.textContent = 'Discover Menu';
 
-  const homeSocials = document.createElement('div');
-  homeSocials.classList.add('home__socials');
-  homeSocials.innerHTML = `<div class="socials__heading">
-  <p class="line"></p>
-  <span>Contact Me</span>
-  </div>`;
-
-  homeLeft.append(headingS, headingB, btnHome, homeSocials)
+  homeLeft.append(headingS, headingB, btnHome);
 
   const homeRight = document.createElement('div');
   homeRight.classList.add('home-right');
 
   homeRight.innerHTML = `<div class="home-right_img">
-  <img src="" alt="" class="home_img">
+  <img src="./pasta.jpg" alt="" class="home_img">
   </div>`;
   home.append(homeRight);
-  
-  // document.querySelector('img.home_img').src = pasta;
 }
 
 function displayMenu() {
@@ -123,13 +112,11 @@ function displayMenu() {
   const menuContent = document.createElement('div');
   menuContent.classList.add('menu__content');
   menu.append(menuContent);
-
- 
 }
 
 function fillInMenu(...args) {
-  args.forEach(arg => {
-    const menuContent = document.querySelector('.menu__content')
+  args.forEach((arg) => {
+    const menuContent = document.querySelector('.menu__content');
     const course = document.createElement('div');
     course.classList.add('course');
     menuContent.append(course);
@@ -138,38 +125,37 @@ function fillInMenu(...args) {
     courseName.classList.add('course-name');
     courseName.textContent = `${arg}`;
     course.append(courseName);
-    
+
     for (let i = 0; i < args.length; i++) {
-      
-    const itemWrapper = document.createElement('div');
-    itemWrapper.classList.add('mi-wrapper');
-    
-    course.append(itemWrapper);
+      const itemWrapper = document.createElement('div');
+      itemWrapper.classList.add('mi-wrapper');
 
-    let argCourse = arg.toLowerCase();
+      course.append(itemWrapper);
 
-    const menuItem = document.createElement('div');
-    menuItem.classList.add('menu-item');
+      let argCourse = arg.toLowerCase();
 
-    const itemName = document.createElement('span');
-    itemName.classList.add('name');
-    itemName.textContent = menu[argCourse][i].name;
+      const menuItem = document.createElement('div');
+      menuItem.classList.add('menu-item');
 
-    const underline = document.createElement('span');
-    underline.classList.add('underline');
+      const itemName = document.createElement('span');
+      itemName.classList.add('name');
+      itemName.textContent = menu[argCourse][i].name;
 
-    const itemPrice = document.createElement('span');
-    itemPrice.classList.add('price');
-    itemPrice.textContent = menu[argCourse][i].price;
+      const underline = document.createElement('span');
+      underline.classList.add('underline');
 
-    menuItem.append(itemName, underline, itemPrice);
+      const itemPrice = document.createElement('span');
+      itemPrice.classList.add('price');
+      itemPrice.textContent = menu[argCourse][i].price;
 
-    const details = document.createElement('p');
-    details.classList.add('details');
-    details.textContent = menu[argCourse][i].details;
+      menuItem.append(itemName, underline, itemPrice);
 
-    itemWrapper.append(menuItem, details)
-    };
+      const details = document.createElement('p');
+      details.classList.add('details');
+      details.textContent = menu[argCourse][i].details;
+
+      itemWrapper.append(menuItem, details);
+    }
   });
 }
 
@@ -184,45 +170,53 @@ function displayContacts(className) {
 
   const contactHeadingS = document.createElement('h6');
   contactHeadingS.classList.add('contact-hs');
-  contactHeadingS.textContent = 'Delivery, catering, events. Just give us a call';
+  contactHeadingS.textContent =
+    'Delivery, catering, events. Just give us a call';
 
   const contacts = document.createElement('div');
   contacts.classList.add('contacts');
   contactUs.append(contactHeadingB, contactHeadingS, contacts);
 
-  for(let i = 1; i < 4; i++) {
-    let contact = document.createElement('div');
-    contact.classList.add(className);
-    contact.innerHTML = contactsData[`contact` + i];
-    contacts.append(contact);
-  };
+  const address = document.createElement('div');
+  address.classList.add(className);
+  const addressImg = document.createElement('img');
+  addressImg.src = './location.png';
+  const addressHeading = document.createElement('p');
+  addressHeading.classList.add('contact-upper');
+  addressHeading.textContent = 'Address';
+  const addressDetails = document.createElement('p');
+  addressDetails.classList.add('contact-details');
+  addressDetails.textContent = '47 Lowe Street';
+  address.append(addressImg, addressHeading, addressDetails);
+
+  const phone = document.createElement('div');
+  phone.classList.add(className);
+  const phoneImg = document.createElement('img');
+  phoneImg.src = './phone.png';
+  const phoneHeading = document.createElement('p');
+  phoneHeading.classList.add('contact-upper');
+  phoneHeading.textContent = 'Phone';
+  const phoneDetails = document.createElement('p');
+  phoneDetails.classList.add('contact-details');
+  phoneDetails.textContent = '111 776 89 90';
+  phone.append(phoneImg, phoneHeading, phoneDetails);
+
+  const email = document.createElement('div');
+  email.classList.add(className);
+  const emailImg = document.createElement('img');
+  emailImg.src = './email.png';
+  const emailHeading = document.createElement('p');
+  emailHeading.classList.add('contact-upper');
+  emailHeading.textContent = 'Email';
+  const emailDetails = document.createElement('p');
+  emailDetails.classList.add('contact-details');
+  emailDetails.textContent = 'italian@mocho.com';
+  email.append(emailImg, emailHeading, emailDetails);
+
+  contacts.append(address, phone, email);
 }
 
 function clearMain() {
   let container = document.querySelector('.container-main');
   container.innerHTML = '';
 }
-
-// window.onload = function() {
-//   displayHome();
-// }
-
-// const navHome = document.querySelector('.nav-home');
-// navHome.addEventListener('click', () => {
-//   clearMain();
-//   displayHome()
-// })
-
-// const navMenu = document.querySelector('.nav-menu');
-// navMenu.addEventListener('click', () => {
-//   clearMain();
-//   displayMenu();
-//   fillInMenu('Mains', 'Pizza', 'Salads')
-// })
-
-// const navContact = document.querySelector('.nav-contact');
-// navContact.addEventListener('click', () => {
-//   clearMain();
-//  displayContacts('contact')
-// })
-
